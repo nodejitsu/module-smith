dc387f1f1104e8ff13178f562cf8a8276c30f89c
indexzero <charlie.robbins@gmail.com>
2013-09-23 18:58:45 -0400

    [minor] Update examples/*.js

--- example/bcrypt.js
+++ example/bcrypt.js
@@ -1,30 +1,37 @@
-var tmp = require('tmp');
-var assert = require('assert');
+/*
+ * bcrypt.js: Example that builds bcrypt.
+ *
+ * (C) 2012 Nodejitsu Inc.
+ *
+ */
 
-var ModuleSmith = require('../');
-var buildbot = ModuleSmith.createModuleSmith();
+var assert = require('assert'),
+    tmp = require('tmp'),
+    moduleSmith = require('../');
+
+var buildbot = moduleSmith.createModuleSmith();
 
 //
 // Grab a temporary directory to build in
 //
 tmp.dir(function (err, tmpdir) {
-   assert.ifError(err);
-   //
-   // Start our build
-   //
-   buildbot.build({
-      repository: {
-         type: 'git',
-         url: 'git@github.com:bmeck/bcrypt-example.git'
-      },
-      directories: {
-         rootdir: tmpdir
-      }
-   }, function (err, stream) {
-      assert.ifError(err);
-      //
-      // Pipe out the data to stdio
-      //
-      stream.pipe(process.stdout);
-   });
+  assert.ifError(err);
+  //
+  // Start our build
+  //
+  buildbot.build({
+    repository: {
+      type: 'git',
+      url: 'git@github.com:bmeck/bcrypt-example.git'
+    },
+    directories: {
+      root: tmpdir
+    }
+  }, function (err, stream) {
+    assert.ifError(err);
+    //
+    // Pipe out the data to stdio
+    //
+    stream.pipe(process.stdout);
+  });
 });
